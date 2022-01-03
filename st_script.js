1ff75995a04318d42939f9ab5268b9c6578ad120
Laurence Putra Franslay <laurenceputra@gmail.com>
2022-01-03 08:17:13 +0800

    Update st_script.js

--- st_script.js
+++ st_script.js
@@ -3,8 +3,8 @@ var phoneArr = {}
 //do the following 3 times after loading all the phones, once for each price tier
 var phones = document.getElementsByClassName("phoneItem");
 for (let i = 0; i < phones.length; i++) {
-    var name = phones[i].getElementsByClassName("phoneModel")[0].innerHTML.trim().replace(" <br>", "");
-    var price = phones[i].getElementsByClassName("phoneCost")[0].innerHTML.trim();
+    var name = phones[i].getElementsByClassName("device--name")[0].innerHTML.trim().replace(" <br>", "");
+    var price = phones[i].getElementsByClassName("price--number")[0].innerHTML.trim();
     if (!phoneArr[name]) {
         phoneArr[name] = []
     }
